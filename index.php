83cf5df68713f04f17fe101280e810fa46e987a6
bariskanat <bkanat@gmail.com>
2014-04-04 16:51:45 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ use Acme\App\Validator;
 $valid=new Validator;
 echo "<pre>";
 
-$valid->make(array("file"=>"baris.txt","firstname"=>"barisk","email"=>"bkanat@gmail"),
+$valid->make(array("file"=>"main.txt","firstname"=>"baris","email"=>"yooremail@gmail"),
               array("file"=>"filesize:3|ext:jpg","firstname"=>"required|min:5|max:20","email"=>"email"))
 ->run();
 $errors=$valid->errors();
